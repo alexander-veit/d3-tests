13d8b209254c15db2192f583ae3871f1746b9773
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2023-08-07 16:04:18 -0400

    Add more center and add table view

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <React.Fragment>
-      <div className='container'>
+      <div className='container pb-5'>
         <ConsortiumMap/>
       </div>
       
